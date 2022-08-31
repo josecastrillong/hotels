84f7b0c1f2f5f1be29102aceb45748afa0341a24
Jose <josecastrillong@gmail.com>
2022-08-30 23:22:08 -0500

    [WIP]

--- src/Components/HotelCard/index.jsx
+++ src/Components/HotelCard/index.jsx
@@ -1,35 +1,24 @@
 import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
 import { Grid } from '@mui/material';
 
 export default function ImgMediaCard() {
   return (
-    <Card>
-      <Grid columns={5}>
-        <CardMedia
-          component="img"
-          alt="green iguana"
-          height="140"
-          image="/static/images/cards/contemplative-reptile.jpg"
-        />
+    <Card sx={{ maxWidth: 700 }}>
+      <Grid container direction="row">
+        <Grid item xs={5}>
+          <CardMedia
+            component="img"
+            alt="green iguana"
+            height="140"
+            image="/static/images/cards/contemplative-reptile.jpg"
+          />
+        </Grid>
+        <Grid item xs={7}>
+          <h5>Miami, USA</h5>
+          <h3>Hotel Hilton</h3>
+        </Grid>
       </Grid>
-      <CardContent>
-        <Typography gutterBottom variant="h5" component="div">
-          Lizard
-        </Typography>
-        <Typography variant="body2" color="text.secondary">
-          Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
-          across all continents except Antarctica
-        </Typography>
-      </CardContent>
-      <CardActions>
-        <Button size="small">Share</Button>
-        <Button size="small">Learn More</Button>
-      </CardActions>
     </Card>
   );
 }
